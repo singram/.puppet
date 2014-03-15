f1e18a313f62031f122be48a24a5030955dca5c6
Stuart Ingram <ingrams2@upmc.com>
2014-03-15 12:19:52 -0400

    Included gtk development packages to ensure building of emacs. Modified mysql package to be more explicit.

--- manifests/system/mysql.pp
+++ manifests/system/mysql.pp
@@ -1,4 +1,6 @@
 package { "mysql":
-    ensure => "5.6.16",
-    source => "http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.16-debian6.0-x86_64.deb"
+    provider => dpkg,
+    ensure   => installed,
+#    source => "http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.16-debian6.0-x86_64.deb"
+    source => "http://downloads.mysql.com/archives/get/file/mysql-5.6.14-debian6.0-x86_64.deb"
 }
