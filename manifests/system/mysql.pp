cdbafccd0920175b4289dfba74a4210527ff31e4
Stuart Ingram <ingrams2@upmc.com>
2014-03-15 18:16:07 -0400

    Mysql updated to download package.

--- manifests/system/mysql.pp
+++ manifests/system/mysql.pp
@@ -1,6 +1,14 @@
+wget::fetch { "mysql_package_download":
+#    source => "http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.16-debian6.0-x86_64.deb"
+    source => "http://downloads.mysql.com/archives/get/file/mysql-5.6.14-debian6.0-x86_64.deb",
+    destination => 'puppet:///packages/mysql-5.6.14-debian6.0-x86_64.deb',
+    timeout     => 0,
+    verbose     => false,
+}
+
 package { "mysql":
     provider => dpkg,
     ensure   => installed,
-#    source => "http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.16-debian6.0-x86_64.deb"
-    source => "http://downloads.mysql.com/archives/get/file/mysql-5.6.14-debian6.0-x86_64.deb"
+    source => "puppet:///packages/mysql-5.6.14-debian6.0-x86_64.deb",
+    before => Wget::Fetch["mysql_package_download"]
 }
