9c147c1d7b51e068fea88179cafe72a3d9fb9102
Stuart Ingram <ingrams2@upmc.com>
2014-03-15 11:32:55 -0400

    Dependency chaining improvements for custom mongo and rabbitmq installations

--- manifests/system/sources.pp
+++ manifests/system/sources.pp
@@ -1,34 +1,36 @@
 include apt
 include wget
 
-apt::source { "puppetlabs":
+apt::source { "puppetlabs_source":
             location        => "http://apt.puppetlabs.com/",
             release         => "precise",
             repos           => "main dependencies",
             include_src     => false
 }
 
-apt::source { "rabbitmq_debian":
+apt::source { "rabbitmq_debian_source":
             location        => "http://www.rabbitmq.com/debian/",
             release         => "testing",
             repos           => "main",
             include_src     => false
 }
 
-apt::source { "mongodb":
+apt::source { "mongodb_source":
             location        => "http://downloads-distro.mongodb.org/repo/ubuntu-upstart",
             release         => "dist",
             repos           => "10gen",
             include_src     => false
 }
 
-apt::key { "rabbitmq":
+apt::key { "rabbitmq_key":
     ensure => present,
     key_source => 'http://www.rabbitmq.com/rabbitmq-signing-key-public.asc',
+    before => Apt::Source['rabbitmq_debian_source']
 }
 
-apt::key { "mongo":
+apt::key { "mongodb_key":
     key => '7F0CEB10',
     ensure => present,
     key_source => 'hkp://keyserver.ubuntu.com:80',
+    before => Apt::Source['mongodb_source']
 }
