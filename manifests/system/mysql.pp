54317173359470e64713bf5ce345844d6eb213f5
Stuart Ingram <ingrams2@upmc.com>
2014-03-15 18:39:01 -0400

    foo

--- manifests/system/mysql.pp
+++ manifests/system/mysql.pp
@@ -7,18 +7,17 @@ file { "/home/$id/packages":
   require => File["/home/$id"]
 }
 
-wget::fetch { "mysql_package_download":
-#    source => "http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.16-debian6.0-x86_64.deb"
-    source => "http://downloads.mysql.com/archives/get/file/mysql-5.6.14-debian6.0-x86_64.deb",
-    destination => "/home/$id/packages/mysql-5.6.14-debian6.0-x86_64.deb",
-    timeout     => 0,
-    verbose     => false,
-    require     => File["/home/$id/packages"]
+exec { "mysql_package_download":
+    path     => "/usr/bin/:/bin/",
+    cwd      =>  "/home/$id/packages/",
+    command  =>  "wget http://downloads.mysql.com/archives/get/file/mysql-5.6.14-debian6.0-x86_64.deb"
+    require     => File["/home/$id/packages"],
+    creates  =>  "/home/$id/packages/mysql-5.6.14-debian6.0-x86_64.deb",
 }
 
-package { "mysql":
+package { "mysql_install":
     provider => dpkg,
     ensure   => installed,
-    source => "puppet:///packages/mysql-5.6.14-debian6.0-x86_64.deb",
+    source => "/home/$id/packages/mysql-5.6.14-debian6.0-x86_64.deb",
     require => Wget::Fetch["mysql_package_download"]
 }
