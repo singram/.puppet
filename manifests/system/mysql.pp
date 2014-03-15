ef574018d4b29ab4d800827e87bd0313700a1cf6
Stuart Ingram <ingrams2@upmc.com>
2014-03-15 18:40:12 -0400

    x

--- manifests/system/mysql.pp
+++ manifests/system/mysql.pp
@@ -1,23 +1,23 @@
 file { "/home/$id":
-  ensure => "directory",
+    ensure   => "directory",
 }
 
 file { "/home/$id/packages":
-  ensure => "directory",
-  require => File["/home/$id"]
+    ensure   => "directory",
+    require  => File["/home/$id"]
 }
 
 exec { "mysql_package_download":
     path     => "/usr/bin/:/bin/",
     cwd      =>  "/home/$id/packages/",
-    command  =>  "wget http://downloads.mysql.com/archives/get/file/mysql-5.6.14-debian6.0-x86_64.deb"
-    require     => File["/home/$id/packages"],
+    command  =>  "wget http://downloads.mysql.com/archives/get/file/mysql-5.6.14-debian6.0-x86_64.deb",
+    require  => File["/home/$id/packages"],
     creates  =>  "/home/$id/packages/mysql-5.6.14-debian6.0-x86_64.deb",
 }
 
 package { "mysql_install":
     provider => dpkg,
     ensure   => installed,
-    source => "/home/$id/packages/mysql-5.6.14-debian6.0-x86_64.deb",
-    require => Wget::Fetch["mysql_package_download"]
+    source   => "/home/$id/packages/mysql-5.6.14-debian6.0-x86_64.deb",
+    require  => Exec["mysql_package_download"]
 }
