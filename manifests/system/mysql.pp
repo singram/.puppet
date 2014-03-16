7d890128a66c1a5a4d133ce28bf98b4488c1649c
Stuart Ingram <ingrams2@upmc.com>
2014-03-15 21:45:07 -0400

    mongo key fix

--- manifests/system/mysql.pp
+++ manifests/system/mysql.pp
@@ -28,7 +28,7 @@ user { "mysql":
 
 file {'/tmp/mysql_installation.sh':
   ensure => file,
-  content => template('manifests/system/mysql_installation.sh'),
+  content => template('manifests/system/mysql_installation.sh.erb'),
   mode => 777,
 }
 
