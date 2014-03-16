8fa7b3e830af16e73574d7625143d084530fbc1f
Stuart Ingram <ingrams2@upmc.com>
2014-03-16 09:04:30 -0400

    Fixes to rvm and adding in additional rvm dependencies.

--- manifests/development/rvm.pp
+++ manifests/development/rvm.pp
@@ -1,13 +1,14 @@
 class rvm {
 
   exec { "rvm_installation":
-     command => "/usr/bin/curl -sSL https://get.rvm.io | bash -s stable",
-     unless => "/bin/which rvm",
+     command     => "/usr/bin/curl -sSL https://get.rvm.io | bash -s stable",
+     unless      => "/bin/which rvm",
+     environment => ["HOME=/home/$id"]
   }
 
   exec { "ruby_1_9_installation":
      command => "/home/$id/.rvm/bin/rvm install ruby-1.9.3-p194 && /home/$id/.rvm/bin/rvm --default use 1.9.3-p194",
-     unless => "/home/$id/.rvm/bin/rvm list | /bin/grep 1.9.3",
+     unless  => "/home/$id/.rvm/bin/rvm list | /bin/grep 1.9.3",
      require => Exec['rvm_installation'],
   }
 
