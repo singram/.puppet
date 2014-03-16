74c5784ea3d1e8ab79ed4ffb951a3f7351856421
Stuart Ingram <ingrams2@upmc.com>
2014-03-16 09:33:45 -0400

    RVM via puppet is challenging.  Taking a simpler route for now.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 =======
 This puppet configuration should allow me to reconstruct my entire development environment on a blank system
 
+Ensure that if you are using Gnome you have changed the terminal preferences to login shell
+
+Edit -> Profiles -> (select profile) Edit -> Title & Command -> Run command as login shell
+
 ## How To Use
 ```
 sudo apt-get update
@@ -9,4 +13,6 @@ sudo apt-get install puppet git
 git clone https://github.com/singram/.puppet.git ~/.puppet
 sudo puppet apply --modulepath=$HOME/.puppet/modules ~/.puppet/manifests/devmachine-root.pp
      puppet apply --modulepath=$HOME/.puppet/modules ~/.puppet/manifests/devmachine.pp
+cd ~/dotfiles
+rake install
 ```
--- manifests/development/dotfiles.pp
+++ manifests/development/dotfiles.pp
@@ -2,15 +2,15 @@ class dotfiles {
 
   exec { 'clone_dotfiles_repo':
       path    => '/usr/bin/:/bin/',
-      command => "git clone https://github.com/singram/dotfiles.git /home/$id/projects/dotfiles",
+      command => "git clone https://github.com/singram/dotfiles.git /home/$id/dotfiles",
       creates => "/home/$id/dotfiles",
   }
 
-  exec { 'dotfile_install':
-      path    => '/usr/bin:/bin/',
-      command => "/home/$id/.rvm/gems/ruby-1.9.3-p194@global/bin/rake install",
-      cwd     => "/home/$id/dotfiles",
-      require => Exec['clone_dotfiles_repo']
-  }
+  # exec { 'dotfile_install':
+  #     path    => '/usr/bin:/bin/',
+  #     command => "/home/$id/.rvm/gems/ruby-1.9.3-p194@global/bin/rake install",
+  #     cwd     => "/home/$id/dotfiles",
+  #     require => Exec['clone_dotfiles_repo']
+  # }
 
 }
