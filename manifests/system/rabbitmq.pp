18a2662dce8ed0a89988a4b2478f39aa67d79d72
singram <singram@singram.com>
2014-09-08 17:10:17 -0400

    Upgraded RabbitMQ to 3.3.4

--- manifests/system/rabbitmq.pp
+++ manifests/system/rabbitmq.pp
@@ -3,21 +3,21 @@ class rabbitmq {
   exec { "rabbitmq_server_package_download":
       cwd      => "/root/packages/",
   #    source => "http://ftp.br.debian.org/debian/pool/main/r/rabbitmq-server/rabbitmq-server_3.1.1-1_all.deb"
-      command  => "wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.1/rabbitmq-server_3.1.1-1_all.deb",
+      command  => "wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.3.4/rabbitmq-server_3.3.4-1_all.deb",
       require  => File["/root/packages"],
       before   => Package["rabbitmq-server"],
-      creates  => "/root/packages/rabbitmq-server_3.1.1-1_all.deb",
+      creates  => "/root/packages/rabbitmq-server_3.3.4-1_all.deb",
   }
 
   package { "rabbitmq-server":
       provider => dpkg,
       ensure   => present,
-      source   => "/root/packages/rabbitmq-server_3.1.1-1_all.deb",
+      source   => "/root/packages/rabbitmq-server_3.3.4-1_all.deb",
       #    unless  => "/usr/bin/dpkg -s rabbitmq-server",
   }
 
   exec { "rabbitmqadmin_installation":
-      command => "wget http://hg.rabbitmq.com/rabbitmq-management/raw-file/rabbitmq_v3_1_1/bin/rabbitmqadmin -P /usr/bin/ && chmod +x /usr/bin/rabbitmqadmin",
+      command => "wget http://hg.rabbitmq.com/rabbitmq-management/raw-file/rabbitmq_v3_3_4/bin/rabbitmqadmin -P /usr/bin/ && chmod +x /usr/bin/rabbitmqadmin",
       creates => "/usr/bin/rabbitmqadmin",
       require => Package["rabbitmq-server"]
   }
