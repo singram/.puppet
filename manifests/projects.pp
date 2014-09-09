b5bc722f6a5fea986d8c66c35c26339c001e4e42
Stuart Ingram <ingrams2@upmc.com>
2014-09-09 15:40:43 -0400

    Added manifest for personal projects

---
+++ manifests/projects.pp
@@ -0,0 +1,3 @@
+import 'projects/projects.pp'
+
+include project_repos
---
+++ manifests/projects/projects.pp
@@ -0,0 +1,31 @@
+class project_repos {
+
+  $personal_dir = "/home/$id/personal"
+
+  file { "/home/$id/personal":
+    ensure => "directory",
+  }
+
+  define personal_repo {
+
+    exec { "clone_${title}_repo":
+        path    => '/usr/bin/:/bin/',
+        command => "git clone https://github.com/singram/${title}.git /${project_repos::personal_dir}/${title}",
+        creates => "/${project_repos::personal_dir}/${title}",
+        require => File[$project_repos::personal_dir]
+    }
+
+  }
+
+  $repos = ['euler',
+            'presentations',
+            'rougelike_ruby',
+            'sentiment',
+            'solution_searching',
+            'cucumber_profiler',
+            'cucumber_rake_runner',
+            'deployment_tracker']
+
+  personal_repo { $repos: }
+
+}
